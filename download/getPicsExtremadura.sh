a001b5d17919bdb0105d617a857cd13364610692
Uwe Schmidt <uwe@fh-wedel.de>
2019-10-28 12:01:56 +0100

    download modified

--- download/getPicsExtremadura.sh
+++ download/getPicsExtremadura.sh
@@ -2,9 +2,10 @@
 
 px="albums/Reisen/Spanien-2019/Slideshow"
 dirs="Gegend Blauelster Bienenfresser Stoerche AmGeierfelsen Mitbewohner Flugstudien"
+# dirs="Gegend Stoerche"
 
 for i in $dirs
 do
     c=$px/$i
-    getcollection.sh $i
+    getcollection.sh $c
 done
