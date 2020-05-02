e09b20c2c7b29f85909fa71dd0b71e95b1989c49
Uwe Schmidt <uwe@fh-wedel.de>
2020-05-02 21:53:12 +0200

    type of ExecProg changed

--- client-polysemy/src/System/ExecProg.hs
+++ client-polysemy/src/System/ExecProg.hs
@@ -24,6 +24,9 @@ module System.ExecProg
     -- * Actions
   , execProg
 
+    -- * Derived Actions
+  , execProg0
+
     -- * Interpreter
   , systemProcess
   )
@@ -34,28 +37,45 @@ import Polysemy.EmbedExc
 import Polysemy.Error
 import Polysemy.Logging
 
-import Control.Monad ( unless )
-import Data.Text     ( Text )
+import Control.Monad   (unless)
+import Data.ByteString (ByteString)
+import Data.Text       (Text)
 import System.Exit
 
-import qualified Data.Text            as T
-import qualified System.IO.Error      as EX
-import qualified System.Process       as X
 
-------------------------------------------------------------------------------
-
-type ProgPath = Text
-
-data ExecProg m a where
-  ExecProg :: ProgPath -> [Text] -> Text -> ExecProg m Text
-
-makeSem ''ExecProg
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Text             as T
+import qualified System.IO.Error       as EX
+import qualified System.Process        as X
 
 ------------------------------------------------------------------------------
 --
 -- | call external program
---
 
+type ProgPath = Text
+
+data ExecProg m a where
+  ExecProg :: ProgPath -> [Text] -> ByteString -> ExecProg m ByteString
+
+makeSem ''ExecProg
+
+execProg0 :: Member ExecProg r
+          => ProgPath -> [Text] -> Sem r ByteString
+execProg0 prg args = execProg prg args mempty
+
+------------------------------------------------------------------------------
+--
+-- | call external program via System.Process.readProcessWithExitCode
+--
+-- prog name and args are given as Text
+-- input and output are represented as a strict ByteString
+--
+-- System.Process works with String as argument and result types
+-- this is inefficient, but it's like it is
+--
+-- Encoding of input and decoding of output is part of
+-- the calling commands, here stdin and stdout get sequences of bytes
+-- char encoding is not handled
 
 systemProcess :: forall exc r
                . ( Member (Embed IO) r
@@ -70,17 +90,20 @@ systemProcess mkExc =
     ExecProg prg args inp -> do
       let prg'  =     T.unpack prg
           args' = map T.unpack args
-          inp'  =     T.unpack inp
+          inp'  =     BS.unpack inp
 
       log'trc $
          T.unwords $
-         ["exec:", prg] <> args <> [", stdin:" <> inp]
+         ["exec:", prg] <> args
+         <> if null inp'
+            then mempty
+            else [", stdin: " <> T.pack inp']
 
       (rc, out', err') <-
          embedExc mkExc $
          X.readProcessWithExitCode prg' args' inp'
 
-      let out = T.pack out'
+      let out = BS.pack out'
           err = T.pack err'
 
       case rc of
