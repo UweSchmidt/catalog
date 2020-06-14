abc62a4c70d07678b9f246887866c1b5536ade51
Uwe Schmidt <uwe@fh-wedel.de>
2020-06-14 19:41:45 +0200

    version bump to 0.3.1.0

--- catalog-client/app/Main.hs
+++ catalog-client/app/Main.hs
@@ -51,10 +51,10 @@ import           System.Exit
 -- version number is updated automatically
 
 version :: String
-version = "0.3.0.4"
+version = "0.3.1.0"
 
 date :: String
-date = "2020-06-12"
+date = "2020-06-14"
 
 appname :: String
 appname = "client"
