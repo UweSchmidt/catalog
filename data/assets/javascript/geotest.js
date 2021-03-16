88fe5b83e0c5d8465109b6f120aa60aa1876d903
Uwe Schmidt <uwe.pe.schmidt@gmail.com>
2021-03-16 10:30:49 +0100

    test file for geoloc added

---
+++ data/assets/javascript/geotest.js
@@ -0,0 +1,27 @@
+{"type":"FeatureCollection",
+ "geocoding":{"version":"0.1.0",
+              "attribution":"Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright",
+              "licence":"ODbL",
+              "query":"29.7,-84.8"
+             },
+ "features":[{"type":"Feature",
+              "properties":{"geocoding":{"place_id":258998279,
+                                         "osm_type":"relation",
+                                         "osm_id":1210712,
+                                         "type":"administrative",
+                                         "accuracy":0,
+                                         "label":"Franklin County, Florida, United States",
+                                         "name":"Franklin County",
+                                         "country":"United States",
+                                         "state":"Florida",
+                                         "admin":{"level4":"Florida",
+                                                  "level6":"Franklin County"
+                                                 }
+                                        }
+                           },
+              "geometry":{"type":"Point",
+                          "coordinates":[-84.8239118,29.8323143]
+                         }
+             }
+            ]
+}^
