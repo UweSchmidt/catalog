57f7e3baf22b3e77d565ad23f75578e17b10d2ab
Uwe Schmidt <uwe@fh-wedel.de>
2022-01-25 13:44:00 +0100

    start-catalog-server extended

--- bin/start-catalog-server.sh
+++ bin/start-catalog-server.sh
@@ -20,6 +20,34 @@ loglevel="--info"
 [[ "$catroot" = "$reldir" ]]         || port="3333"  # develop dir
 [[ "$catroot" = "$reldir" ]]         || loglevel="--verbose"
 
+# options: -P --port, -j --journal, -v --verbose, -t --trace
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -P|--port)
+            port="$2"  # overwrite default port 3001 or 3333
+            shift
+            shift
+            ;;
+        -j|--journal)
+            journal="$2"
+            shift
+            shift
+            ;;
+        -i|--info|-v|--verbose|-t|--trace|-w|--warnings|-q|--quiet|--errors)
+            loglevel="$1"
+            shift
+            ;;
+        -*|--*)
+            die "Unknown option: $1"
+            ;;
+        *)
+            die "Illegal argument(s): $*"
+            ;;
+    esac
+done
+
+
 [[ "$(basename $PWD)" = "catalog" ]] || die "current working dir must be 'catalog' "
 
 [[ -d "data" ]]                      || die "no subdir data"
@@ -30,6 +58,8 @@ server="../bin/servant-polysemy"
 catdir="catalog"
 catalog="photos-0.5.pathid.json"
 
+[[ "$journal" != "" ]]               || journal="${catdir}-journal-$(date "+%Y-%m-%dT%H:%M:%S").json"
+
 [[ -d "$catdir" ]]                   || die "no subdir '$catdir'"
 [[ -f "$catdir/$catalog" ]]          || die "no catalog '$catdir/$catalog'"
 [[ -x "$server" ]]                   || die "no executable '$server'"
@@ -38,5 +68,5 @@ exec $server \
      $loglevel \
      -P "$port" \
      -a "$catdir/$catalog" \
-     -j "${catdir}-journal-$(date "+%Y-%m-%dT%H:%M:%S").json" \
+     -j "$journal" \
      --save-hash-and-path-ix
