55af77f9674f7f8147b5e978317c57c7f10ba81e
Uwe Schmidt <uwe@fh-wedel.de>
2022-10-07 18:15:53 +0200

    new .js module Catalog.MetaData

--- data/assets/javascript/show.js
+++ data/assets/javascript/show.js
@@ -1,5 +1,11 @@
+// ----------------------------------------
+
 // import Prelude
 // import Data.V2
+// import Data.Geo
+// import DOM.Manipulate
+// import Catalog.Urls
+// import Catalog.MetaData
 
 /* ---------------------------------------- */
 /* id's */
@@ -1867,149 +1873,6 @@ function keyPressed (e) {
 document.addEventListener("keypress", editKeyPressed);
 document.addEventListener("keyup",    keyUp);
 // document.addEventListener("resize",   resizedScreen);
-
-// ----------------------------------------
-// build metadata table
-
-const metaInfo = {
-    "Descr:Title":                  "Titel",
-    "Descr:Subtitle":               "Untertitel",
-    "Descr:TitleEnglish":           "Titel engl.",
-    "Descr:TitleLatin":             "Titel lat.",
-    "Descr:Comment":                "Kommentar (Aufnahme)",
-    "Descr:CommentImg":             "Kommentar (Kopie)",
-    "EXIF:CreateDate":              "Aufnahmedatum",
-    "Descr:Address":                "Adresse",
-    "Descr:Location":               "Ort",
-    // "Descr:GPSPosition":            "Position",
-    "Descr:GPSPositionDeg":         "Position in Grad",
-    "Descr:GPSAltitude":            "Höhe",
-    "Descr:Web":                    "Web",
-    "Descr:Wikipedia":              "Wikipedia",
-    "Descr:Keywords":               "Schlüsselwörter",
-    "EXIF:Model":                   "Kamera",
-    "Composite:LensSpec":           "Objektiv",
-    "Composite:LensID":             "Objektiv Typ",
-    "EXIF:FocalLength":             "Brennweite",
-    "EXIF:FocalLengthIn35mmFormat": "Brennweite in 35mm",
-    "EXIF:ExposureTime":            "Belichtungszeit",
-    "EXIF:FNumber":                 "Blende",
-    "EXIF:ExposureCompensation":    "Belichtungskorrektur",
-    "EXIF:ISO":                     "ISO",
-    "EXIF:ExposureMode":            "Belichtungsmessung",
-    "EXIF:ExposureProgram":         "Aufnahmebetriebsart",
-    "MakerNotes:FocusDistance":     "Entfernung",
-    "Composite:DOF":                "Tiefenschärfe",
-    "Composite:FOV":                "Sichtfeld",
-    "Composite:HyperfocalDistance": "Hyperfokale Distanz",
-    "MakerNotes:ShootingMode":      "Aufnahmemodus",
-    "EXIF:WhiteBalance":            "Weißabgleich",
-    "MakerNotes:ShutterCount":      "Aufnahmezähler",
-    "Composite:ImageSize":          "Geometrie",
-    "File:RefRaw":                  "Raw-Datei",
-    "File:RefImg":                  "Bilddatei",
-    "File:MimeType":                "Dateityp",
-    "Composite:Megapixels":         "Megapixel",
-    "File:FileSize":                "Dateigröße",
-    "File:RefJpg":                  "Bild-Kopie",
-    "Gif:AnimationIterations":      "Animation: Wiederh.",
-    "Gif:Duration":                 "Animation: Dauer",
-    "Gif:FrameCount":               "Animation: # Bilder",
-    "QuickTime:Duration":           "Video-Dauer",
-    "QuickTime:VideoFrameRate":     "Frame-Rate",
-    "File:DateTime":                "Bearbeitet",
-    "Descr:Rating":                 "Bewertung"
-};
-
-// the function table for formating values
-// default is newText(v)
-
-const metaFmt = {
-    "Composite:FOV":        fmtDeg,
-    "Composite:Megapixels": fmtMPX,
-    "Descr:GPSPositionDeg": fmtGPS,
-    "Descr:Rating":         fmtRating,
-    "Descr:Web":            fmtWeb,
-    "Descr:Wikipedia":      fmtWeb
-};
-
-function lookupFmt(k) {
-    return metaFmt[k] || newText;
-}
-
-function fmtMPX(t) {
-    return newText("" + (1 * t));
-}
-
-function fmtDeg(t) {
-    const deg = String.fromCharCode(176);
-    return newText(t.replace(/ deg/g, deg));  // regex to replace all occourences
-}
-
-var gpsUrl;  // hack: global var for 2. parameter
-
-function fmtGPS(t) {
-    const a   = newElem("a");
-    const txt = fmtDeg(t);
-
-    a.href    = gpsUrl;
-    a.target  = "_blank";
-    a.classList.add("gpslink");
-    a.appendChild(txt);
-    return a;
-}
-
-function toDegree(t) {
-    trc(1, "toDegree: " + t);
-    return t;
-}
-
-function fmtRating(n) {
-    const star  = String.fromCharCode(9733);
-    const stars = replicate(1 * n, star);   // conversion to int
-    const txt   = newText(stars);
-    const spn   = newElem("span");
-    spn.classList.add("rating");
-    spn.appendChild(txt);
-    return spn;
-}
-
-function fmtWeb(url) {
-    const txt = newText(url);
-    const a   = newElem("a");
-    a.href    = url;
-    a.target  = "_blank";
-    a.classList.add("weblink");
-    a.appendChild(txt);
-    return a;
-}
-
-function buildMetaInfo (t, md) {
-    // clear metadata table
-    clearCont(t);
-
-    // hack for google maps url
-    gpsUrl = md["Descr:GPSurl"];
-
-    for (k in metaInfo) {
-        const v = md[k];
-        if ( v ) {
-            // build key div
-            const kw = newElem("div", "", "key");
-            const kx = newText(metaInfo[k]);
-            kw.appendChild(kx);
-
-            // build value div
-            const vl = newElem("div", "", "value");
-            const tx = lookupFmt(k)(md[k]);
-            vl.appendChild(tx);
-
-            // insert key-value into info table
-            t.appendChild(kw);
-            t.appendChild(vl);
-        }
-    }
-}
 
 // ----------------------------------------
 
