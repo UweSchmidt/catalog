21d3b008e6c394fb50c8cb5165dd8e645ac8318c
Uwe Schmidt <uwe.pe.schmidt@gmail.com>
2025-09-17 09:39:28 +0200

    external-copy: options added

--- bin/external-catalog.sh
+++ bin/external-catalog.sh
@@ -2,6 +2,10 @@
 
 # set -x
 
+# no uninitialized variables
+
+set -u
+
 # rsync all media files from "Diakaesten" with a media backup
 
 function die() {
@@ -12,6 +16,31 @@ function die() {
 function trc() {
     echo '#' "$@" 1>&2
 }
+
+xOpt=
+tOpt=
+volumeOpt="5TB-Diakasten"
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -x)
+            xOpt="-x"
+            shift
+            ;;
+        -t|--test)
+            tOpt="-t"
+            shift
+            ;;
+        --volume)
+            shift
+            volumeOpt="$1"
+            shift
+            ;;
+        *)
+            die "Illegal option(s) / argument(s): $* "
+            ;;
+    esac
+done
 
 # compute physical dir path of this script
 res=""
@@ -29,46 +58,40 @@ function physDir() {
 
 orgCat="/Volumes/8TB-SieGeht/home/uwe/Bilder/catalog"
 devCat="/Users/uwe/haskell/catalog"
-extCat="/Volumes/5TB-Diakasten/home/uwe/Bilder/catalog"
+extCat="/Volumes/$volumeOpt/home/uwe/Bilder/catalog"
 
 physDir "$0"
 curCat=$(dirname "$res")
 
-function isTest() { [[ "yes"   = "yes" ]]; }
+function isTest() { [[ "$tOpt"  = "-t" ]]; }
+function isExec() { [[ "$xOpt"  = "-x" ]]; }
 
-function isOrg() { [[ "$curCat" == "$orgCat" ]] || isTest; }
-function isDev() { [[ "$curCat" == "$devCat" ]] || isTest; }
-function isExt() { [[ "$curCat" == "$extCat" ]] || isTest; }
+function isOrg() { [[ "$curCat" = "$orgCat" ]] || isTest; }
+function isDev() { [[ "$curCat" = "$devCat" ]] || isTest; }
+function isExt() { [[ "$curCat" = "$extCat" ]] || isTest; }
 
 isOrg || isDev || isExt || die "unknown catalog dir: $curCat"
 
 if isTest
 then
-    defaultCatSrc="$HOME/haskell/catalog"
-    defaultCatDst="$HOME/tmp/catalog"
-    defaultSrcDir="$HOME/haskell/Diakaesten"
+    catSrc="$HOME/haskell/catalog"
+    catDst="$HOME/tmp/catalog"
+    srcDir="$HOME/haskell/Diakaesten"
 else
-    defaultDisk="5TB-Diakasten"
-    defaultSrcDir="$HOME/Bilder/Diakaesten"
-    defaultCatDst="/Volumes/$defaultDisk/home/uwe/Bilder/catalog"
+    catSrc="$curCat"
+    srcDir="$HOME/Bilder/Diakaesten"
+    catDst="$extCat"
 fi
+dstDir="$catDst/photos"
 
 dry="--dry-run"
 DRY="echo DRY-RUN> "
 
-
-if [[ "$1" = "-x" ]]
+if isExec
 then
-    run=1
-    shift
     dry=
     DRY=
 fi
-
-catSrc="$defaultCatSrc"
-catDst="$defaultCatDst"
-srcDir="$defaultSrcDir"
-dstDir="$catDst/photos"
 
 [[ -d "$srcDir" ]] || die "media source directory not fount: $srcDir"
 [[ -d "$catDst" ]] || die "catalog destination directory not fount: $dstDir"
@@ -90,9 +113,10 @@ done
 trc copy all media files from $srcDir to $dstDir
 trc rsync $dry -av $excludes "$srcDir/" "$dstDir"
 
+# no .-files and no files not used by catalog server
 isOrg && \
     rsync $dry -av \
-          --exclude='.DS_Store' \
+          --exclude='.*' \
           $excludes \
           "$srcDir/" "$dstDir"
 
@@ -101,24 +125,24 @@ dstData="$catDst/data"
 
 trc copy catalog data from $srcData/ to $dstData
 
+# git repos and configs (.git, .gitignore) are includd
 isOrg && \
     rsync $dry -av \
-          --exclude='.*' \
+          --exclude='.DS_Store' \
           --exclude='*~' \
           --exclude='dist-newstyle' \
           --exclude='catalog-journal*.json' \
           "$srcData/" "$dstData"
 
-srcBin=$catSrc/bin
-dstBin=$catDst/bin
 
 trc copy catalog sources and binaries from $catSrc/ to $catDst
 
+# git repos and configs (.git, .gitignore) are included
 ( isOrg || isDev ) && \
     rsync $dry -av \
-      --exclude='.*' \
-      --exclude='*~' \
-      --exclude='data' \
-      --exclude='dist-newstyle' \
-      --exclude='catalog-journal*.json' \
-      "$catSrc/" "$catDst"
+          --exclude='.DS_Store' \
+          --exclude='*~' \
+          --exclude='data' \
+          --exclude='dist-newstyle' \
+          --exclude='catalog-journal*.json' \
+          "$catSrc/" "$catDst"
